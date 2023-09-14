2cd32507abbbb0425a8e4d7cc604adddec2a5dbc
Zelly Snyder <zelly@outlook.com>
2023-09-14 05:46:43 -0700

    create fn to align data series with datetimes

--- src/commodity_info.ts
+++ src/commodity_info.ts
@@ -1,3 +1,4 @@
+import next from 'next/types';
 import {CommodityCodes, IPriceFeed} from './cftc_codes_mapping';
 import { requestFredSeries } from './fred_api';
 import { LHAssert } from './util';
@@ -15,40 +16,44 @@ export async function getPriceFeed(priceFeed: IPriceFeed) {
 
 export interface DatetimeAlignmentContainer<T> {
     // `T` is a data type that has a Date or Datetime type in it somewhere
-    data: T[],
+    items: T[],
     // getDate is how you retrieve the 'Date' contained in `data`
-    getDate: (x: T) => Date,
+    getDate: (item: T) => Date,
     // return a new T that sets the contained date to `newDate`
-    setDate: (x: T, newDate: Date) => T,
+    setDate: (item: T, newDate: Date) => T,
 };
 
-export function alignDatetimes<T, Q>(src: DatetimeAlignmentContainer<T>, fitToTarget: DatetimeAlignmentContainer<Q>, toleranceMillis: number): T[] {
-    if (src.data.length === 0 || fitToTarget.data.length === 0) {
+export function alignDatetimes<T, Q>(src: DatetimeAlignmentContainer<T>, fitToTarget: DatetimeAlignmentContainer<Q>): T[] {
+    if (src.items.length === 0 || fitToTarget.items.length === 0) {
         return [];
     }
-    let output: T[] = [];
-    const targetDatetimes = fitToTarget.data.map((x: Q) => fitToTarget.getDate(x)).sort((a, b) => b.getTime() - a.getTime());
-    let srcData = [...src.data].sort((a: T, b: T) => src.getDate(b).getTime() - src.getDate(a).getTime());
-    let srcIdx = 0;
-    let targetIdx = 0;
-    while (srcIdx < srcData.length && targetIdx < targetDatetimes.length) {
-        const srcTime = src.getDate(srcData[srcIdx]).getTime();
-        const refDtsTime = targetDatetimes[targetIdx].getTime();
-        const timeDiffMillis = Math.abs(srcTime - refDtsTime);
-        if (timeDiffMillis <= toleranceMillis) {
-            const res = src.setDate(srcData[srcIdx], targetDatetimes[targetIdx]);
-            output.push(res);
-            targetIdx++;
-            srcIdx++;
-            continue;
+
+    let dst: T[] = [];
+    // prepare the input series data
+    // sort in descending order
+    const targetData = [...fitToTarget.items].sort((a: Q, b: Q) => fitToTarget.getDate(b).getTime() - fitToTarget.getDate(a).getTime());
+    let srcData = [...src.items].sort((a: T, b: T) => src.getDate(b).getTime() - src.getDate(a).getTime());
+
+    // using the `fitToTarget` dates, look for the entries in `src` with nearest dates
+    for (let targetIdx = 0, srcIdx = 0; srcIdx < srcData.length && targetIdx < targetData.length; ++targetIdx) {
+        // advance to the next item in `src` that is closest in time to this `targetTime`
+        let minimalTimeDiff = Infinity; // Math.abs(src.getDate(srcData[srcIdx]).getTime() - fitToTarget.getDate(targetData[targetIdx]).getTime());
+        for (let srcIdx2 = srcIdx; srcIdx2 < srcData.length; ++srcIdx2) {
+            let maybeMinimalTimeDiff = Math.abs(src.getDate(srcData[srcIdx2]).getTime() - fitToTarget.getDate(targetData[targetIdx]).getTime());
+            if (maybeMinimalTimeDiff > minimalTimeDiff) {
+                // because the series are sorted by time, assume the time differences will keep getting smaller on each iteration
+                // so the encounter with a larger time difference indicates the rest of the series will be even larger
+                // there's no need to check the rest of the series after this one
+                break;
+            }
+            if (maybeMinimalTimeDiff <= minimalTimeDiff) {
+                minimalTimeDiff = maybeMinimalTimeDiff;
+                srcIdx = srcIdx2;
+            }
         }
-        if (srcTime > refDtsTime) {
-            srcIdx++;
-        } else if (refDtsTime > srcTime) {
-            targetIdx++;
-        } else {
-            throw new Error('should be unreachable');
-        }
+        const result = src.setDate(srcData[srcIdx], fitToTarget.getDate(targetData[targetIdx]));
+        dst.push(result);
     }
-    return output.reverse();
+
+    return dst.reverse();
 }
