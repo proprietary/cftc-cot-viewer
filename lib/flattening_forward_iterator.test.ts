1f03540dfeecfa9d049a3e55b0338fc952e54650
Zelly Snyder <zelly@outlook.com>
2023-09-25 04:49:32 -0700

    add new utility type along with tests

---
+++ lib/flattening_forward_iterator.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "@jest/globals";
+import FlatteningForwardIterator from "./flattening_forward_iterator";
+
+describe('flattening forward iterator', () => {
+    test('works for numbers', () => {
+        const a1: readonly number[] = [1, 2, 3];
+        const a2: readonly number[] = [4, 5, 6, 7];
+        let result = [];
+        for (const it of new FlatteningForwardIterator(a1, a2)) {
+            result.push(it);
+        }
+        expect(result).toEqual([1, 2, 3, 4, 5, 6, 7]);
+    });
+
+    test('handles empty', () => {
+        let it = new FlatteningForwardIterator();
+        expect(Array.from(it)).toEqual([]);
+    });
+
+    test('handles single array', () => {
+        expect(Array.from(new FlatteningForwardIterator(["one"]))).toEqual(["one"]);
+    });
+})
---
+++ lib/flattening_forward_iterator.ts
@@ -0,0 +1,36 @@
+export default class FlatteningForwardIterator<T> implements Iterable<T> {
+    private aggregated: Array<readonly T[]>;
+    private outerIdx = 0;
+    private innerIdx = 0;
+
+    constructor(...arrays: Array<readonly T[]>) {
+        this.aggregated = arrays;
+    }
+
+    [Symbol.iterator](): Iterator<T> {
+        return this;
+    }
+
+    public next(): IteratorResult<T> {
+        if (this.outerIdx >= this.aggregated.length) {
+            return {
+                done: true,
+                value: undefined as any,
+            };
+        }
+        if (this.innerIdx >= this.aggregated[this.outerIdx].length) {
+            this.outerIdx += 1;
+            this.innerIdx = 0;
+        }
+        if (this.outerIdx >= this.aggregated.length) {
+            return {
+                done: true,
+                value: undefined as any,
+            };
+        }
+        return {
+            done: false,
+            value: this.aggregated[this.outerIdx][this.innerIdx++],
+        };
+    }
+}
